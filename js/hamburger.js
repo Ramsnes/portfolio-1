e8ba3a4427d0678613b7396f13ad9d7b246917ec
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2023-10-08 21:52:31 +0200

    fixed logos instead of buttons

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,14 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarlinks = document.getElementsByClassName("navbar-links")[0];
+const mobileMenuLinks = document.querySelectorAll(".navbar-links ul li a");
 
 toggleButton.addEventListener("click", () => {
   navbarlinks.classList.toggle("active"); //creates class active, which is styled
+});
+
+// click event listeners to clear hamMenu
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbarlinks.classList.remove("active"); // Remove the 'active' class to close the mobile menu
+  });
 });
